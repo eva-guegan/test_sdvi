82b44a6598a92675e4dfde716d057c7860715834
Eva Guegan <eva.guegan@hotmail.fr>
2020-05-27 15:29:48 +0200

    Avancement calcul du prix de fabrication d'une pizza

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Controller;
 
+use App\Entity\IngredientPizza;
 use App\Repository\PizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -46,8 +47,24 @@ class PizzaController extends AbstractController
     {
         $detailPizza = $pizzaRepo->findPizzaAvecDetailComplet($pizzaId);
 
+        foreach ($detailPizza as $item) {
+            $item['quantite'];
+        }
+
+        $quantite = [80, 105, 45, 60, 30, 48, 105];
+        $prix = [3.7, 10.72, 7.9, 6.3, 2.68, 12.38, 12.32];
+
+        for ($i=0, $size = count($quantite); $i<$size; $i++) {
+            $qteEnKg[] = $quantite[$i] * 0.001;
+            $calcul[] = $qteEnKg[$i] * $prix[$i];
+        }
+
+        $total = array_sum($calcul);
+
         return $this->render("Pizza/detail.html.twig", [
             "detailPizza" => $detailPizza,
+            "calcul" => $calcul,
+            "total" => $total
         ]);
     }
 }
