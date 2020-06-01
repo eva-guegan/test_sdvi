c032f2faa399bd4e92e1f96b6fa3f8ef7d090347
Eva Guegan <eva.guegan@hotmail.fr>
2020-06-01 15:04:55 +0200

    Travail sur encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
-require('../css/app.css');
+import '../css/app.css';
+
+import $ from 'jquery';
+import greet from './greet';
+
+$(document).ready(function() {
+    $('body').prepend('<h1>'+greet('jill')+'</h1>');
+});
